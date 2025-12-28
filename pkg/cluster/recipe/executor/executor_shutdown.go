df210629d1d4604c7be2116a552075d76b6d83f9
Harish Bohara <harish.bohara@gmail.com>
2025-12-29 00:16:51 +0530

    added provider in module for easy main

--- pkg/cluster/recipe/executor/executor_shutdown.go
+++ pkg/cluster/recipe/executor/executor_shutdown.go
@@ -6,6 +6,7 @@ import (
 	"github.com/devlibx/gox-base/v2/errors"
 	helixWorkerMysql "github.com/devlibx/gox-helix/pkg/cluster/recipe/worker/database"
 	"github.com/devlibx/gox-helix/pkg/common/config"
+	"log/slog"
 )
 
 func (s *serviceImpl) Stop(ctx context.Context) error {
@@ -26,6 +27,8 @@ func (s *serviceImpl) stopDomainOnStop(ctx context.Context, domain *config.Domai
 		},
 	); err != nil {
 		return errors.Wrap(err, fmt.Sprintf("failed to deregister worker=%s for domain=%s", s.workerId, domain.Name))
+	} else {
+		slog.Info("[SHUTDOWN] worker deregistered successfully on shoutdown", "domain", domain.Name, "workerId", s.workerId)
 	}
 	return nil
 }
