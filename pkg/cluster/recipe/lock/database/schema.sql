6bc7933ceee4812bc2096c8a61d2ba15b4b3bc42
Harish Bohara <harish.bohara@gmail.com>
2025-12-30 01:56:09 +0530

    fixed tests for lock - the UNIQUE KEY was wrong

--- pkg/cluster/recipe/lock/database/schema.sql
+++ pkg/cluster/recipe/lock/database/schema.sql
@@ -10,6 +10,6 @@ CREATE TABLE helix_locks
     `created_at` datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `lock_key_status_unique_key` (`lock_key`, `status`),
+    UNIQUE KEY `lock_key_status_unique_key` (`domain`, `lock_key`, `status`),
     KEY `lock_key_ids` (`lock_key`)
 );
